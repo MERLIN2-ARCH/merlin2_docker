94d044c2e0559d0c34991b457a1fad227ce1c3ba
Miguel Ángel González Santamarta <mgons@unileon.es>
2023-03-26 15:35:54 +0200

    rb1_sandbox added

--- src/install/rb1.sh
+++ src/install/rb1.sh
@@ -3,6 +3,8 @@
 # clone
 cd /root/ros2_ws/src
 git clone -b humble https://github.com/mgonzs13/ros2_rb1.git
+git clone --recurse-submodules https://github.com/mgonzs13/ros2_rb1_sandbox.git
+echo "GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:~/ros2_ws/install/aws_robomaker_hospital_world/share/aws_robomaker_hospital_world/fuel_models/" >>/root/.bashrc
 
 # rosdep
 source /opt/ros/humble/setup.bash
